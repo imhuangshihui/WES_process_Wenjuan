2a4af3cc69c7e643b2aa3282e1f9bafd6617bfd3
June HUANG <76728625+imhuangshihui@users.noreply.github.com>
2023-05-25 11:06:28 +0800

    Create bwa.PWH55.sh

---
+++ 02.BAM/PWH55/bwa.PWH55.sh
@@ -0,0 +1,14 @@
+cd /mnt/hwstor9k_data1/ccgmtest/work/PWHA55pedigree/02.BAM/PWH55
+date '+--- BWA MEM Start %y-%m-%d %H:%M:%S' >PWH55.bwa.log
+####Allowable options are ILLUMINA,SLX,SOLEXA,SOLID,454,LS454,COMPLETE,PACBIO,IONTORRENT,CAPILLARY,HELICOS,UNKNOWN
+####java -jar AddOrReplaceReadGroups.jar I=sample.dedup.realn.bam O=sample.dedup.realn.RG.bam SO=coordinate RGID=sample RGLB=sample RGPU=sample RGPL=ILLUMINA RGSM=sample
+/mnt/hwstor9k_data1/ccgmtest/work/software/bwa-0.7.17/bwa mem -t 48 -M -R "@RG\tID:PWH55\tSM:PWH55\tLB:PWH55\tPL:ILLUMINA" /mnt/hwstor9k_data1/ccgmtest/work/software/PubData/hg38/Homo_sapiens_assembly38.fasta /mnt/hwstor9k_data1/ccgmtest/work/PWHA55pedigree/01.cleanData/PWH55/PWH55.paired_R1.fq.gz /mnt/hwstor9k_data1/ccgmtest/work/PWHA55pedigree/01.cleanData/PWH55/PWH55.paired_R2.fq.gz | /mnt/hwstor9k_data1/ccgmtest/work/software/samtools-1.9/samtools view -bS -t /mnt/hwstor9k_data1/ccgmtest/work/software/PubData/hg38/Homo_sapiens_assembly38.fasta.fai -o PWH55.bam - && 
+date '+--- BWA MEM END %y-%m-%d %H:%M:%S' >>PWH55.bwa.log
+/mnt/hwstor9k_data1/ccgmtest/work/software/samtools-1.9/samtools sort -m 3000000000 PWH55.bam -T /mnt/hwstor9k_data1/ccgmtest/work/PWHA55pedigree/02.BAM/PWH55/tmp -o PWH55.sorted.bam 1>> PWH55.bwa.log 2>&1 || exit 1
+/mnt/hwstor9k_data1/ccgmtest/work/software/samtools-1.9/samtools index PWH55.sorted.bam 1>> PWH55.bwa.log 2>&1 || exit 1
+rm -f PWH55.bam
+#rm -f /mnt/hwstor9k_data1/ccgmtest/work/PWHA55pedigree/01.cleanData/PWH55/PWH55.paired_R1.fq.gz /mnt/hwstor9k_data1/ccgmtest/work/PWHA55pedigree/01.cleanData/PWH55/PWH55.paired_R2.fq.gz 
+date '+--- BAM sort END %y-%m-%d %H:%M:%S' >>PWH55.bwa.log
+echo 'PWH55 PWH55  BWA Finished %y-%m-%d %H:%M:%S' >> /mnt/hwstor9k_data1/ccgmtest/work/PWHA55pedigree/02.BAM/bwa_finish.result
+
+sh gatk.sh 
